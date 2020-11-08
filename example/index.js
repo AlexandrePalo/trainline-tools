bdeb640ef4d2a6c61f3785b1d0b037fffe8e1ece
Alexandre PALO <a-palo@laposte.net>
2020-11-08 21:09:05 +0100

    add searchLocations example

--- example/index.js
+++ example/index.js
@@ -1,3 +1,36 @@
-const { searchJourneys } = require('../dist/bundle')
+const ora = require('ora')
+const chalk = require('chalk')
+const { searchLocations } = require('../dist/bundle')
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
 
-searchJourneys()
+console.log(chalk`
+    {bold TRAINLINE TOOLS}
+    {bold Search Location objects}
+`)
+readline.question(`Enter term: `, (term) => {
+    console.log(chalk`
+    Search Locations objects matching {red ${term}}
+    `)
+
+    const spinner = ora('Loading data').start()
+    searchLocations(term).then((data) => {
+        spinner.stop()
+        console.log(chalk`
+        {red ${data.length}} location${data.length > 1 && 's'} found!
+        `)
+        console.log(
+            chalk`${data.slice(0, 5).reduce((displayed, l, i) => {
+                if (i === 4 && data.length > 5) {
+                    return `${displayed}, ${l.name}, and more...`
+                } else if (i > 0) {
+                    return `${displayed}, ${l.name}`
+                } else {
+                    return l.name
+                }
+            }, '')}`
+        )
+    })
+})
